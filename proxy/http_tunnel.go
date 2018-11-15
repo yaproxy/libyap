512ea271c8c89b878026debaec8783f725e3fadc
楚贤 <chuxian.mjj@antfin.com>
2018-11-15 19:06:51 +0800

    add http post tunnel support

---
+++ proxy/http_tunnel.go
@@ -0,0 +1,19 @@
+package proxy
+
+import (
+	"net"
+	"github.com/jim3ma/http-tunnel"
+)
+
+func httpTunnel(network, schema, addr string, auth *Auth, forward Dialer, resolver Resolver) (Dialer, error) {
+	var host string
+	var port string
+
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, err
+	}
+	d := ht.NewTunnel(schema, host, port, "/ping", "/pong")
+
+	return d, nil
+}

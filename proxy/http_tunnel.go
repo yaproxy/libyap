8cf81ec1017f36477dc2fb4d24d6e7df2b3b275c
楚贤 <chuxian.mjj@antfin.com>
2018-11-16 14:23:48 +0800

    add auth for http post tunnel

--- proxy/http_tunnel.go
+++ proxy/http_tunnel.go
@@ -2,6 +2,8 @@ package proxy
 
 import (
 	"net"
+	"net/url"
+
 	"github.com/jim3ma/http-tunnel"
 )
 
@@ -13,7 +15,7 @@ func httpTunnel(network, schema, addr string, auth *Auth, forward Dialer, resolv
 	if err != nil {
 		return nil, err
 	}
-	d := ht.NewTunnel(schema, host, port, "/ping", "/pong")
+	d := ht.NewTunnel(schema, host, port, "/ping", "/pong", url.UserPassword(auth.User, auth.Password))
 
 	return d, nil
 }
